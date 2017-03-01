7e0f9e8d97647bab8d4e29489ecaa6b41e439671
Janek Brandt <brandt.janek@gmail.com>
2017-02-28 19:25:18 -0800

    add paddle constructor and prototype to move paddle up and down on keypress

---
+++ README.md
@@ -0,0 +1,33 @@
+# _Pong_
+
+#### _Pong, 2-28-2017_
+
+#### By _**Janek Brandt**_
+
+## Description
+_This project is and implementation of Pong using html canvas and JS._
+
+
+## Specifications
+
+| Behavior                   | Input Example     | Output Example    |
+| -------------------------- | -----------------:| -----------------:|
+| create a Field object and draw it on canvas | | |
+| draw a player paddle on the canvas | | |
+| draw a ball on the canvas | | |
+| method to move player paddle up and down | | |
+| method to move ball based on direction and speed | | |
+
+
+
+## Setup/Installation Requirements
+
+* _Clone the repository_
+* _Open index.html file in web browser to view the project locally_
+* _Use web server of your choice to host the website_
+
+### License
+
+Copyright (c) 2017 **_Janek Brandt_**
+
+This software is licensed under the MIT license.
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 // Back-End
-function Field() {
-
+function Field(width, height) {
+  this.width = width;
+  this.height = height;
 };
+
+function Paddle(field) {
+  this.height = field.height/5;
+  this.width = 10;
+  this.x = 5;
+  this.y = field.height/2 - this.height/2;
+};
+
+Paddle.prototype.moveUp = function() {
+  this.y -= 5;
+}
+
+Paddle.prototype.moveDown = function() {
+  this.y += 5;
+}
+
+Paddle.prototype.draw = function(ctx) {
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
 
 function Ball() {
 
@@ -12,6 +32,32 @@ function Ball() {
 // Front-End
 $(function() {
   var canvas = document.getElementById('canvas');
-  var ctx - canvas.getContext('2d');
+  var ctx = canvas.getContext('2d');
+  var width = canvas.width;
+  var height = canvas.height;
 
+  var field = new Field(width, height);
+  var player1 = new Paddle(field);
+
+  document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 38) {
+      player1.moveUp();
+      drawGame();
+    } else if (event.keyCode === 40) {
+      player1.moveDown();
+      drawGame();
+    };
+  }, false);
+
+
+  function drawGame() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = 'white';
+    player1.draw(ctx);
+  }
+
+  drawGame();
+  // ctx.fillRect(5, ((height/2) - (player1.height/2)), 10, player1.height);
+  // console.log(player1.getPos(field));
 })
