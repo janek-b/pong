2e6e7987b2856b684f05b06d2ad712df5eebacc5
Janek Brandt <brandt.janek@gmail.com>
2017-02-28 18:10:41 -0800

    add initial directory structure. add html canvas and outline for game objects

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Back-End
+function Field() {
+
+};
+
+function Ball() {
+
+};
+
+
+
+// Front-End
+$(function() {
+  var canvas = document.getElementById('canvas');
+  var ctx - canvas.getContext('2d');
+
+})
